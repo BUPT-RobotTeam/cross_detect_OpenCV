216742c68414a5a3f777e9c6733560c900111f7e
woshiren <yejiacong@outlook.com>
2023-09-14 22:13:04 +0800

    feat: success detect cross

--- cross_detect.py
+++ cross_detect.py
@@ -53,116 +53,26 @@ def rad2deg(rad):
     return rad / np.pi * 180
 
 
-# frame = cv2.imread('./test.jpg')
-# result = pre_processing(frame)
-# edges = cv2.Canny(result, 0, 0, apertureSize=3)
-# lines = cv2.HoughLines(edges, 1, np.pi / 180, 75)
-# vertical_lines = []
-# horizontal_lines = []
-# if lines is not None:
-#     for line in lines:
-#         rho, theta = line[0]
-#         theta1 = rad2deg(theta)
-#         print(theta1)
-#         if 120 >= theta1 >= 70:
-#             print("vertical")
-#             a = np.cos(theta)
-#             b = np.sin(theta)
-#             x0 = a * rho
-#             y0 = b * rho
-#             slope = -a / b if b != 0 else np.inf
-#             intercept = y0 - slope * x0
-#             if slope != np.inf:
-#                 vertical_lines.append([slope, intercept])
-#                 x1 = int(x0 + 1000 * (-b))
-#                 y1 = int(y0 + 1000 * a)
-#                 x2 = int(x0 - 1000 * (-b))
-#                 y2 = int(y0 - 1000 * a)
-#                 cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 0), 2)
-#             else:
-#                 vertical_lines.append([slope, x0])
-#                 cv2.line(frame, (int(x0), 0), (int(x0), 480), (0, 0, 0), 2)
-#         elif 0 <= theta1 <= 30 or theta1 >= 150:
-#             print ("horizontal")
-#             a = np.cos(theta)
-#             b = np.sin(theta)
-#             x0 = a * rho
-#             y0 = b * rho
-#             slope = -a / b if b != 0 else np.inf
-#             intercept = y0 - slope * x0
-#             if slope != np.inf:
-#                 horizontal_lines.append([slope, intercept])
-#                 x1 = int(x0 + 1000 * (-b))
-#                 y1 = int(y0 + 1000 * a)
-#                 x2 = int(x0 - 1000 * (-b))
-#                 y2 = int(y0 - 1000 * a)
-#                 cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-#             else:
-#                 horizontal_lines.append([slope, x0])
-#                 cv2.line(frame, (int(x0), 0), (int(x0), 480), (0, 0, 255), 2)
-#
-# ax = ay = 0
-#
-# for line in vertical_lines:
-#     slope, intercept = line
-#     x = y = 0
-#     if slope != np.inf:
-#         x, y = get_mid_point(slope, intercept)
-#     else:
-#         x = intercept
-#         y = 240
-#
-#     ax += x
-#     ay += y
-#
-# ax /= len(vertical_lines)
-# ay /= len(vertical_lines)
-#
-# cv2.circle(frame, (int(ax), int(ay)), 5, (0, 0, 0), -1)
-#
-# bx = 0
-# by = 0
-#
-# for line in horizontal_lines:
-#     slope, intercept = line
-#     x = y = 0
-#     if slope != np.inf:
-#         x, y = get_mid_point(slope, intercept)
-#     else:
-#         x = intercept
-#         y = 240
-#
-#     bx += x
-#     by += y
-#
-# bx /= len(horizontal_lines)
-# by /= len(horizontal_lines)
-#
-# cv2.circle(frame, (int(bx), int(by)), 5, (0, 0, 255), -1)
-#
-# cv2.circle(frame, (int((ax + bx) / 2), int((ay + by) / 2)), 5, (255, 0, 0), -1)
-#
-# cv2.imshow('edges', edges)
-# cv2.imshow('frame', frame)
-
-# cv2.waitKey(0)
-
-
 while True:
     _, frame = video.read()
     frame = undistort.undistort(undistort.s908_params, frame)
     result = pre_processing(frame)
     edges = cv2.Canny(result, 0, 0, apertureSize=3)
     lines = cv2.HoughLines(edges, 1, np.pi / 180, 75)
-    vertical_lines = []
-    horizontal_lines = []
+    vertical_lines = [[],[]]
+    vertical_lines_cnt = [0, 0]
+    vertical_lines_rho = [0, 0]
+    horizontal_lines = [[],[]]
+    horizontal_lines_cnt = [0, 0]
+    horizontal_lines_rho = [0, 0]
     if lines is not None:
         for line in lines:
             rho, theta = line[0]
-            theta1 = rad2deg(theta)
-            print(theta1)
-            if 120 >= theta1 >= 70:
-                print("vertical")
+            theta_deg = rad2deg(theta)
+            print(theta_deg)
+            if 120 >= theta_deg >= 70:
+                if vertical_lines_cnt[0] == 0:
+                    vertical_lines_rho[0] = rho
                 a = np.cos(theta)
                 b = np.sin(theta)
                 x0 = a * rho
@@ -170,17 +80,30 @@ while True:
                 slope = -a / b if b != 0 else np.inf
                 intercept = y0 - slope * x0
                 if slope != np.inf:
-                    vertical_lines.append([slope, intercept])
+                    cur_index = 0
+                    if abs(rho - vertical_lines_rho[0]) > 50:
+                        cur_index = 1
+                        if vertical_lines_cnt[1] == 0:
+                            vertical_lines_rho[1] = rho
+                    vertical_lines[cur_index].append([slope, intercept])
+                    vertical_lines_cnt[cur_index] += 1
                     x1 = int(x0 + 1000 * (-b))
                     y1 = int(y0 + 1000 * a)
                     x2 = int(x0 - 1000 * (-b))
                     y2 = int(y0 - 1000 * a)
-                    cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 0), 2)
+                    cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 0) if cur_index==0 else (255,0,0), 2)
                 else:
-                    vertical_lines.append([slope, x0])
-                    cv2.line(frame, (int(x0), 0), (int(x0), 480), (0, 0, 0), 2)
-            elif 0 <= theta1 <= 30 or theta1 >= 150:
-                print ("horizontal")
+                    cur_index = 0
+                    if abs(rho - vertical_lines_rho[0]) > 50:
+                        cur_index = 1
+                        if vertical_lines_cnt[1] == 0:
+                            vertical_lines_rho[1] = rho
+                    vertical_lines[cur_index].append([slope, x0])
+                    vertical_lines_cnt[cur_index] += 1
+                    cv2.line(frame, (int(x0), 0), (int(x0), 480), (0, 0, 0) if cur_index==0 else (255,0,0) , 2)
+            elif 0 <= theta_deg <= 30 or theta_deg >= 150:
+                if horizontal_lines_cnt[0] == 0:
+                    horizontal_lines_rho[0] = rho
                 a = np.cos(theta)
                 b = np.sin(theta)
                 x0 = a * rho
@@ -188,26 +111,38 @@ while True:
                 slope = -a / b if b != 0 else np.inf
                 intercept = y0 - slope * x0
                 if slope != np.inf:
-                    horizontal_lines.append([slope, intercept])
+                    cur_index = 0
+                    if abs(rho - horizontal_lines_rho[0]) > 50:
+                        cur_index = 1
+                        if horizontal_lines_cnt[1] == 0:
+                            horizontal_lines_rho[1] = rho
+                    horizontal_lines[cur_index].append([slope, intercept])
+                    horizontal_lines_cnt[cur_index] += 1
                     x1 = int(x0 + 1000 * (-b))
                     y1 = int(y0 + 1000 * a)
                     x2 = int(x0 - 1000 * (-b))
                     y2 = int(y0 - 1000 * a)
-                    cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                    cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255) if cur_index==0 else (0,255,0), 2)
                 else:
+                    cur_index = 0
+                    if abs(rho - horizontal_lines_rho[0]) > 50:
+                        cur_index = 1
+                        if horizontal_lines_cnt[1] == 0:
+                            horizontal_lines_rho[1] = rho
                     horizontal_lines.append([slope, x0])
-                    cv2.line(frame, (int(x0), 0), (int(x0), 480), (0, 0, 255), 2)
+                    horizontal_lines_cnt[cur_index] += 1
+                    cv2.line(frame, (int(x0), 0), (int(x0), 480), (0, 0, 255) if cur_index==0 else (0,255,0) , 2)
 
     ax = ay = 0
 
-    if (len(vertical_lines) == 0) or (len(horizontal_lines) == 0):
+    if (len(vertical_lines[0]) == 0) or (len(horizontal_lines[0]) == 0) or (len(vertical_lines[1]) == 0) or (len(horizontal_lines[1]) == 0):
         cv2.imshow('edges', edges)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         continue
 
-    for line in vertical_lines:
+    for line in vertical_lines[0]:
         slope, intercept = line
         x = y = 0
         if slope != np.inf:
@@ -219,15 +154,15 @@ while True:
         ax += x
         ay += y
 
-    ax /= len(vertical_lines)
-    ay /= len(vertical_lines)
+    ax /= len(vertical_lines[0])
+    ay /= len(vertical_lines[0])
 
-    cv2.circle(frame, (int(ax), int(ay)), 5, (0, 0, 0), -1)
+    cv2.circle(frame,(int(ax), int(ay)), 5, (0, 255, 255), -1)
 
     bx = 0
     by = 0
 
-    for line in horizontal_lines:
+    for line in vertical_lines[1]:
         slope, intercept = line
         x = y = 0
         if slope != np.inf:
@@ -239,12 +174,54 @@ while True:
         bx += x
         by += y
 
-    bx /= len(horizontal_lines)
-    by /= len(horizontal_lines)
+    bx /= len(vertical_lines[1])
+    by /= len(vertical_lines[1])
 
-    cv2.circle(frame, (int(bx), int(by)), 5, (0, 0, 255), -1)
+    cv2.circle(frame,(int(bx), int(by)), 5, (0, 255, 255), -1)
 
-    cv2.circle(frame, (int((ax + bx) / 2), int((ay + by) / 2)), 5, (255, 0, 0), -1)
+    vertical_mid_point = [(ax + bx) / 2, (ay + by) / 2]
+
+    ax = ay = 0
+
+    for line in horizontal_lines[0]:
+        slope, intercept = line
+        x = y = 0
+        if slope != np.inf:
+            x, y = get_mid_point(slope, intercept)
+        else:
+            x = intercept
+            y = 320
+
+        ax += x
+        ay += y
+
+    ax /= len(horizontal_lines[0])
+    ay /= len(horizontal_lines[0])
+
+    cv2.circle(frame,(int(ax), int(ay)), 5, (255, 0, 255), -1)
+
+    bx = by = 0
+
+    for line in horizontal_lines[1]:
+        slope, intercept = line
+        x = y = 0
+        if slope != np.inf:
+            x, y = get_mid_point(slope, intercept)
+        else:
+            x = intercept
+            y = 320
+
+        bx += x
+        by += y
+
+    bx /= len(horizontal_lines[1])
+    by /= len(horizontal_lines[1])
+
+    cv2.circle(frame,(int(bx), int(by)), 5, (255, 0, 255), -1)
+
+    horizontal_mid_point = [(ax + bx) / 2, (ay + by) / 2]
+
+    cv2.circle(frame,(int((horizontal_mid_point[0]+vertical_mid_point[0])/2), int((horizontal_mid_point[1]+vertical_mid_point[1])/2)), 5, (114, 514, 0xA), -1)
 
     cv2.imshow('edges', edges)
     cv2.imshow('frame', frame)
